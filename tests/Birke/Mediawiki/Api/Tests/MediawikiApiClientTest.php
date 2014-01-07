b495a68b885ba7d09812fbc9dc68bac17569f13d
Gabriel Birke <birke@d-scribe.de>
2014-01-07 15:05:59 +0100

    Add parse API call
    Extend JSON service description
    Test two call variants with Wikipedia data

--- tests/Birke/Mediawiki/Api/Tests/MediawikiApiClientTest.php
+++ tests/Birke/Mediawiki/Api/Tests/MediawikiApiClientTest.php
@@ -8,9 +8,7 @@ class MediawikiApiClientTest extends \Guzzle\Tests\GuzzleTestCase
     public function testHelp()
     {
         $client = $this->getServiceBuilder()->get('client');
-        
         $command = $client->getCommand('help');
-        
         $response = $client->execute($command);
 
         $this->assertArrayHasKey('error', $response);
@@ -21,13 +19,53 @@ class MediawikiApiClientTest extends \Guzzle\Tests\GuzzleTestCase
     public function testModuleHelp()
     {
         $client = $this->getServiceBuilder()->get('client');
-        
         $command = $client->getCommand('help', array('modules' => 'opensearch|parse'));
-        
         $response = $client->execute($command);
 
         $this->assertArrayHasKey('help', $response);
         $this->assertCount(2, $response['help']);
+    }
+    
+    public function testParsePage() {
+        $client = $this->getServiceBuilder()->get('client');
+        $command = $client->getCommand('parse', array('page' => 'Wiki'));
+        $response = $client->execute($command);
+        
+        $this->assertArrayHasKey('parse', $response);
+        $this->assertArrayHasKey('title', $response['parse']);
+        $this->assertArrayHasKey('revid', $response['parse']);
+        $props = array(
+            "langlinks",
+            "categories",
+            "links",
+            "templates",
+            "images",
+            "externallinks",
+            "sections",
+            "revid",
+            "displaytitle",
+            "properties"
+        );
+        foreach($props as $p) {
+            $this->assertArrayHasKey($p, $response['parse']);
+        }
+        $this->assertEquals('Wiki', $response['parse']['title']);
+    }
+    
+    public function testParseText() {
+        $client = $this->getServiceBuilder()->get('client');
+        $testtext = "= Wiki = \n This is test text. \n\nSecond Paragraph\n\n== Foo ==\nLorem Ipsum";
+        $command = $client->getCommand('parse', array('text' => $testtext, 'contentmodel' => 'wikitext'));
+        $response = $client->execute($command);
+        
+        $this->assertArrayHasKey('parse', $response);
+        $this->assertArrayHasKey('text', $response['parse']);
+        $this->assertArrayHasKey('*', $response['parse']['text']);
+        $this->assertArrayHasKey('title', $response['parse']);
+        $this->assertArrayHasKey('sections', $response['parse']);
+        $this->assertEquals('API', $response['parse']['title']);
+        $this->assertGreaterThan(strlen($testtext), strlen($response['parse']['text']['*']));
+        $this->assertCount(2, $response['parse']['sections']);
     }
     
 }
